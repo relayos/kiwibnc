3f5604787b618d8ec4f2f6bf31821e0348a1f355
relaxgg-devops <devops-sa@relax.gg>
2026-05-17 18:23:34 +0000

    feat: support sql-backed user database

--- src/libs/database.js
+++ src/libs/database.js
@@ -1,6 +1,76 @@
 const path = require('path');
 const knex = require('knex');
 
+function decodeConnectionPart(value) {
+    return decodeURIComponent(value.replace(/\+/g, '%20'));
+}
+
+function parseMysqlConnectionString(connectionString) {
+    const scheme = connectionString.startsWith('mariadb://') ? 'mariadb://' : 'mysql://';
+    const withoutScheme = connectionString.slice(scheme.length);
+    const authEnd = withoutScheme.lastIndexOf('@');
+    if (authEnd === -1) {
+        throw new Error('Invalid mysql connection string: missing credentials');
+    }
+
+    const auth = withoutScheme.slice(0, authEnd);
+    const hostPath = withoutScheme.slice(authEnd + 1);
+    const userEnd = auth.indexOf(':');
+    if (userEnd === -1) {
+        throw new Error('Invalid mysql connection string: missing password');
+    }
+
+    const slash = hostPath.indexOf('/');
+    const hostPort = slash === -1 ? hostPath : hostPath.slice(0, slash);
+    const databaseAndQuery = slash === -1 ? '' : hostPath.slice(slash + 1);
+    const queryStart = databaseAndQuery.indexOf('?');
+    const database = queryStart === -1 ?
+        databaseAndQuery :
+        databaseAndQuery.slice(0, queryStart);
+    const query = queryStart === -1 ? '' : databaseAndQuery.slice(queryStart + 1);
+
+    let host = hostPort;
+    let port;
+    if (hostPort.startsWith('[')) {
+        const hostEnd = hostPort.indexOf(']');
+        host = hostEnd === -1 ? hostPort : hostPort.slice(1, hostEnd);
+        if (hostEnd !== -1 && hostPort[hostEnd + 1] === ':') {
+            port = Number(hostPort.slice(hostEnd + 2));
+        }
+    } else {
+        const portStart = hostPort.lastIndexOf(':');
+        if (portStart !== -1) {
+            host = hostPort.slice(0, portStart);
+            port = Number(hostPort.slice(portStart + 1));
+        }
+    }
+
+    const connection = {
+        host: decodeConnectionPart(host),
+        user: decodeConnectionPart(auth.slice(0, userEnd)),
+        password: decodeConnectionPart(auth.slice(userEnd + 1)),
+        database: decodeConnectionPart(database),
+    };
+    if (port) {
+        connection.port = port;
+    }
+
+    const params = new URLSearchParams(query);
+    if (params.has('charset')) {
+        connection.charset = params.get('charset');
+    }
+
+    return connection;
+}
+
+function isMysqlConnectionString(connectionString) {
+    return !!connectionString &&
+        (
+            connectionString.startsWith('mysql://') ||
+            connectionString.startsWith('mariadb://')
+        );
+}
+
 module.exports = class Database {
     constructor(config) {
         let dbConf = config.get('database', {});
@@ -21,7 +91,7 @@ module.exports = class Database {
             connection: null,
             acquireConnectionTimeout: 10000,
         };
-        if (usersConStr.indexOf('postgres://') > -1) {
+        if (usersConStr.startsWith('postgres://')) {
             // postgres://someuser:somepassword@somehost:381/somedatabase
             usersDbCon.client = 'pg';
             usersDbCon.connection = usersConStr;
@@ -29,10 +99,13 @@ module.exports = class Database {
             if (searchPathM) {
                 usersDbCon.searchPath = searchPathM[1].split(',');
             }
-        } else if (usersConStr.indexOf('mysql://') > -1) {
+        } else if (isMysqlConnectionString(usersConStr)) {
             // mysql://user:password@127.0.0.1:3306/database
-            // knex handles this connection string internally
-            usersDbCon = usersConStr;
+            usersDbCon = {
+                client: 'mysql',
+                connection: parseMysqlConnectionString(usersConStr),
+                acquireConnectionTimeout: 10000,
+            };
         } else {
             // No scheme:// part in the connection string, assume it's an sqlite filename
             usersDbCon.client = 'better-sqlite3';
