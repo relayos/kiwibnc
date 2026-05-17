e5a96becf81c02f5b4e71378c330b669ddf1c8c2
relaxgg-devops <devops-sa@relax.gg>
2026-05-17 18:33:55 +0000

    chore: retain deployed relaybnc migration stubs

---
+++ src/dbschemas/users/20260516190000_wordpress_user_fk.js
@@ -0,0 +1,7 @@
+exports.up = async function() {
+    // Compatibility stub: RelayOS WordPress linkage now lives in the webchat overlay.
+};
+
+exports.down = function() {
+    // Never go backwards in the db.
+};
